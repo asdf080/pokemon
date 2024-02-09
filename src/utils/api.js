04d295d12f0d8160101d8a81ec465fd5f09ad788
Dahee Lee <ai04120@naver.com>
2024-02-09 16:20:48 +0900

    card

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,5 @@ const base = "https://pokeapi.co/api/v2/";
 export async function apiGetPokes({ endpoint }) {
   try {
     return await fetch(base + endpoint).then((res) => res.json());
-  } catch (error) {
-    console.error(error);
-  }
+  } catch (error) {}
 }
