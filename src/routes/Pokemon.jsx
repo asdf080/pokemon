e8f186b6dfd3a4e283c867f4f0fd5be5500b32e9
Dahee Lee <ai04120@naver.com>
2024-02-11 02:30:42 +0900

    info

--- src/routes/Pokemon.jsx
+++ src/routes/Pokemon.jsx
@@ -11,6 +11,11 @@ export default function Pokemon() {
   const { data: 기본정보, isLoading: 기본로딩중 } = useQuery({ queryKey: ["pokeInfo", id], queryFn: () => apiGetPokes({ endpoint: `pokemon/${id}` }) });
   const { data: 세부정보, isLoading: 세부로딩중 } = useQuery({ queryKey: ["pokeInfo2", id], queryFn: () => apiGetPokes({ endpoint: `pokemon-species/${id}` }) });
   const [sprImg, setSprImg] = useState(null);
+  const { data: 성별정보 } = useQuery({
+    queryKey: ["pokeInfo3", id],
+    queryFn: () => apiGetPokes({ endpoint: `gender/1` }),
+  });
+  const 성비 = ((성별정보?.pokemon_species_details?.find((item) => item?.pokemon_species?.name === 기본정보.name).rate / 8) * 100).toFixed(1);
 
   useEffect(() => {
     if (!기본로딩중 && 기본정보) {
@@ -39,6 +44,21 @@ export default function Pokemon() {
 
   console.log(세부정보);
 
+  const abilityQueries = useQueries({
+    queries:
+      기본정보?.abilities?.map((item) => ({
+        queryKey: ["ability", item.ability.name],
+        queryFn: async () => {
+          try {
+            const data = await fetch(item.ability.url).then((res) => res.json());
+            return data;
+          } catch (error) {}
+        },
+      })) ?? [],
+  });
+
+  const 특성 = abilityQueries?.map((result) => result.data ?? {});
+
   return (
     <Layout>
       {기본로딩중 || !sprImg ? (
@@ -47,9 +67,9 @@ export default function Pokemon() {
         </div>
       ) : (
         <section key={id} id="pokeDetail">
-          <h2>{세부정보?.names?.find((item) => item.language.name == "ko")?.name || 기본정보.name}</h2>
+          <h2>{세부정보?.names?.find((item) => item.language?.name == "ko")?.name || 기본정보.name}</h2>
           <h3>
-            <span>{세부정보?.genera?.find((item) => item.language.name == "ko")?.genus || "???"}</span>
+            <span>{세부정보?.genera?.find((item) => item.language?.name == "ko")?.genus || "???"}</span>
           </h3>
 
           <article id="detail1">
@@ -60,9 +80,12 @@ export default function Pokemon() {
               <div className="leftTxt">
                 {기본정보?.height * 10}cm &nbsp;&nbsp; {기본정보?.weight}kg
               </div>
-              <div className="leftTit">기초 경험치</div>
-              <div className="leftTxt">{기본정보?.base_experience}</div>
-
+              <div className="leftTit">특성</div>
+              <div className="leftTxt">
+                {특성?.map((item, index) => (
+                  <p key={index}>{item.names?.find((name) => name.language.name === "ko").name}</p>
+                ))}
+              </div>
               <div className="leftTit">타입</div>
               <div className="leftTxt">
                 <p className={`${기본정보?.types[0].type.name} typeIcon`}>{기본정보?.types[0].type.name}</p>
@@ -70,25 +93,29 @@ export default function Pokemon() {
               </div>
               <div className="leftTit">형태</div>
               <div className="leftTxt">
-                {세부정보?.varieties.map((item) => (
-                  <p className="variIcon">{item.pokemon.name}</p>
+                {세부정보?.varieties.map((item, index) => (
+                  <p key={index} className="variIcon">
+                    {item.pokemon.name.replace(기본정보.name, 세부정보?.names?.find((item) => item.language.name == "ko")?.name || 기본정보.name)}
+                  </p>
                 ))}
               </div>
               {기본정보?.sprites.other[`official-artwork`].front_shiny && <button onClick={이미지전환}>다른 색상</button>}
             </div>
-            <img key={id} src={sprImg.url} alt="artwork" />
+            <img key={id} src={`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${id.padStart(3, "0")}.png`} alt="artwork" />
             <div id="rightTxtWrap">
-              {기본정보?.stats?.map((item, index) => (
-                <div key={index}>
-                  <div className="progName">{item.stat.name.replace("special-", "sp. ")}</div>
-                  <div className="progress">
-                    <div style={{ width: `${(item.base_stat / 최대스탯) * 100}%` }}>{item[`base_stat`]}</div>
+              <div id="rightTxtWrapInner">
+                {기본정보?.stats?.map((item, index) => (
+                  <div key={index} className="progWrap">
+                    <div className="progName">{item.stat.name.replace("special-", "sp. ")}</div>
+                    <div className="progress">
+                      <div style={{ width: `${(item.base_stat / 최대스탯) * 100}%` }}>{item[`base_stat`]}</div>
+                    </div>
                   </div>
+                ))}
+                <div className="progWrap">
+                  <p className="progName">total</p>
+                  <p>{기본정보?.stats?.reduce((a, b) => a + b.base_stat, 0)}</p>
                 </div>
-              ))}
-              <div>
-                <p className="progName">total</p>
-                <p>{기본정보?.stats?.reduce((a, b) => a + b.base_stat, 0)}</p>
               </div>
             </div>
           </article>
@@ -101,12 +128,34 @@ export default function Pokemon() {
                 <div>{기본정보?.held_items?.[0]?.item?.name || "없음"}</div>
                 <div className="underTit">서식지</div>
                 <div>{세부정보?.habitat.name}</div>
-                <div className="underTit">성장 속도</div>
-                <div>{세부정보?.growth_rate.name}</div>
+                <div className="underTit">색상</div>
+                <div>{세부정보?.color.name}</div>
                 <div className="underTit">친밀도</div>
                 <div>{세부정보?.base_happiness}</div>
                 <div className="underTit">포획률</div>
                 <div>{세부정보?.capture_rate}</div>
+              </div>
+            </div>
+            <div className="underWrap">
+              <h5>육성 정보</h5>
+              <div className="underTxt">
+                <div className="underTit">기초 경험치</div>
+                <div className="">{기본정보?.base_experience}</div>
+                <div className="underTit">알 그룹</div>
+                <div>
+                  {세부정보?.egg_groups.map((item, index) => (
+                    <p key={index}>{item.name}</p>
+                  ))}
+                </div>
+                <div className="underTit">부화 카운트</div>
+                <div>{세부정보?.hatch_counter}</div>
+                <div className="underTit">성장 속도</div>
+                <div>{세부정보?.growth_rate.name}</div>
+                <div className="underTit">성비</div>
+                <div>
+                  <p>{성비}%</p>
+                  <p>{100 - 성비}%</p>
+                </div>
               </div>
             </div>
           </article>
