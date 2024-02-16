3cb82c4df64510303384c2c4983cec07fb2b8a01
Dahee Lee <ai04120@naver.com>
2024-02-16 23:13:53 +0900

    w

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import Layout from "../components/Layout";
 
 export default function Write() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <section id="writeSect"></section>
+    </Layout>
+  );
 }
