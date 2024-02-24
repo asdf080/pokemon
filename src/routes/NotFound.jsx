73713c43bb06367e9fdb7e472ead81a66e807ee6
Dahee Lee <ai04120@naver.com>
2024-02-24 17:24:53 +0900

    404

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -11,8 +11,8 @@ export default function NotFound() {
     <Layout>
       <section id="notFound" style={{ height: "85vh", width: "80vw", margin: "0 auto", textAlign: "center", textTransform: "uppercase" }}>
         <Lottie animationData={notFound} style={{ height: "60vh" }} />
-        <h2 style={{ margin: "20px 0 0", fontSize: "2em" }}>페이지를 찾을 수 없습니다.</h2>
-        <button style={{ margin: "15px auto 0", border: "none", padding: "10px 25px", fontSize: "1.2em", borderRadius: "10px", display: "flex", alignItems: "center", background: "#fff5e2" }} onClick={() => navi(-1)}>
+        <h2 style={{ margin: "30px 0 0", fontSize: "2em" }}>페이지를 찾을 수 없습니다.</h2>
+        <button style={{ margin: "15px auto 0", border: "2px solid #ffcf00", padding: "10px 25px", fontSize: "1.2em", borderRadius: "10px", display: "flex", alignItems: "center", background: "none" }} onClick={() => navi(-1)}>
           <IoArrowBackOutline />
           &nbsp; 뒤로 가기
         </button>
